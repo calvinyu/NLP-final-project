420c26913e221d10d513f5d9a4afad2ec5eeaf51
Calvin Yu <ycy247@nyu.edu>
2015-04-22 13:25:48 -0400

    add all sentenses

--- sentenseSegmentor.py
+++ sentenseSegmentor.py
@@ -1,5 +1,9 @@
 import pickle
 import nltk
+
+"""
+This module 
+"""
 def read():
   return pickle.loads(open('extract_data/review_quotes', 'r').read())
 def segment(raw):
@@ -7,6 +11,7 @@ def segment(raw):
   return sents
 
 if __name__ == "__main__":
+  allSentence = []
   rqPairs = read()
   documents = []
   for rq in rqPairs:
@@ -15,9 +20,11 @@ if __name__ == "__main__":
     sents = segment(r)
     doc = []
     for sent in sents:
+      allSentence.append(sent)
       if q in sent:
         doc.append((1, sent))
       else:
         doc.append((0, sent))
     documents.append(doc)
   pickle.dump(documents, open('dataset', 'w'))
+  pickle.dump(allSentence, open('allSent', 'w'))
