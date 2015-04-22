1041b4213a4d3f9e211ffcbe58765ecac94a551a
Calvin Yu <ycy247@nyu.edu>
2015-04-21 22:07:46 -0400

    add movie link extractor

---
+++ movieLinkQuoteInventory.py
@@ -0,0 +1,29 @@
+import pickle
+##############################
+#
+# This module extract all the movie quotes
+# and their original article links from
+# rotten tomatos api
+#
+# Usage:
+# getLinkQuote() returns a list of 
+# dictionary of key u'link' and u'quote'
+#
+##############################
+datas = pickle.loads(open('Review_dict', 'r').read())
+result = []
+for data in datas:
+  for review in datas[data]['reviews']:
+    
+    try:
+      link = review['links']['review'] 
+      if 'http://www.moviechambers.com' in link or \
+          'www.macleans.ca' in link or \
+          'www.film4.com' in link or \
+          'http://www.flicks.co.nz'in link:
+            result.append({'link': link, 'quote':review['quote']})
+    except:
+      pass
+def getLinkQuote():
+  global result
+  return result
