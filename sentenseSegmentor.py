a95b280d546766cc79719cb858625342d4b1da7f
Calvin Yu <ycy247@nyu.edu>
2015-04-27 23:30:50 -0400

    Fix: unicode bug I used some kind of heuristic way to tag the conclusion sentenses!! There weill be 1 ~ 4 sentences tagged!!

--- sentenseSegmentor.py
+++ sentenseSegmentor.py
@@ -26,22 +26,40 @@ def read():
 def segment(raw):
   sents = nltk.sent_tokenize(raw)
   return sents
+def match(s, q):
+  length = len(q)
+  cnt = 0
+  for i in range(0, length-7,7):
+    if q[i:i+15] in s:
+      cnt += 1
+  if cnt > 3:
+    return True
+  return False
 
 if __name__ == "__main__":
   allSentence = []
   rqPairs = read()
   documents = []
+  cnt = 0
+  i = 0
   for rq in rqPairs:
     r = rq['review']
+    r = r.replace(u'\xa0', u' ')
+    r = r.replace(u'\u201c', u'"')
+    r = r.replace(u'\u201d', u'"')
     q = rq['quote']
     sents = segment(r)
     doc = []
+    i += 1
+    cc = 0
     for sent in sents:
       allSentence.append(sent)
-      if q in sent:
+      if match(sent, q):
+        cnt += 1
         doc.append((1, sent))
       else:
         doc.append((0, sent))
     documents.append(doc)
+  print cnt, len(rqPairs)
   pickle.dump(documents, open('dataset', 'w'))
   pickle.dump(allSentence, open('allSent', 'w'))
